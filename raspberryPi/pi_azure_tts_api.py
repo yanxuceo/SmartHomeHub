d99260869a8c434c887cf3d86c415f80ca1a9c39
xu dong <yanxuceo@gmail.com>
2023-08-17 21:44:22 +0200

    added direct audio play when tts is done; cleaned up the long array parts

--- raspberryPi/pi_azure_tts_api.py
+++ raspberryPi/pi_azure_tts_api.py
@@ -4,6 +4,87 @@ import azure.cognitiveservices.speech as speechsdk
 import xml.etree.ElementTree as ET
 import argparse
 
+import pyaudio
+import wave
+
+all_voice = [
+    'en-US-AnaNeural',
+    'en-AU-CarlyNeural',
+    'en-GB-MaisieNeural',
+    'here starts FEMALE',
+    'en-US-JaneNeural',
+    'en-US-JennyNeural', 
+    'en-US-MichelleNeural', 
+    'en-US-NancyNeural',   
+    'en-US-SaraNeural', 
+    'en-AU-FreyaNeural', 
+    'en-AU-KimNeural', 
+    'en-AU-NatashaNeural', 
+    'en-GB-LibbyNeural',
+    'en-GB-SoniaNeural',
+    'en-HK-YanNeural',
+    'here starts MALE',
+    'en-US-AIGenerate1Neural',
+    'en-US-BrandonNeural',
+    'en-US-ChristopherNeural',
+    'en-US-DavisNeural',
+    'en-US-EricNeural',
+    'en-US-GuyNeural',
+    'en-US-JacobNeural',
+    'en-US-RogerNeural',
+    'en-CA-LiamNeural',
+    'en-US-JasonNeural',
+    'en-US-SteffanNeural', 
+    'en-US-TonyNeural',
+    'en-AU-NeilNeural', 
+    'en-AU-WilliamNeural',
+    'en-GB-OliverNeural', 
+    'en-GB-RyanNeural',
+    'here starts CHINESE',
+    'zh-CN-sichuan-YunxiNeural',
+    'zh-CN-shandong-YunxiangNeural',
+    'zh-CN-shaanxi-XiaoniNeural',
+    'zh-CN-liaoning-XiaobeiNeural',
+    'zh-CN-henan-YundengNeural',
+    'zh-CN-XiaochenNeural',
+    'zh-CN-XiaohanNeural',
+    'zh-CN-XiaomengNeural',
+    'zh-CN-XiaomoNeural',
+    'zh-CN-XiaoqiuNeural',
+    'zh-CN-XiaoruiNeural',
+    'zh-CN-XiaoshuangNeural',
+    'zh-CN-XiaoxiaoNeural',
+    'zh-CN-XiaoxuanNeural',
+    'zh-CN-XiaoyanNeural',
+    'zh-CN-XiaoyiNeural',
+    'zh-CN-XiaoyouNeural',
+    'zh-CN-XiaozhenNeural',
+    'zh-CN-YunfengNeural',
+    'zh-CN-YunhaoNeural',
+    'zh-CN-YunjianNeural',
+    'zh-CN-YunxiaNeural',
+    'zh-CN-YunxiNeural',
+    'zh-CN-YunyangNeural',
+    'zh-CN-YunyeNeural',
+    'zh-CN-YunzeNeural'
+]
+
+which_speed_to_use = ['0.5', '0.7', '0.8', '0.9', '1.0', '1.2', '1.4', '1.6', '1.8', '2.0']
+
+emotion_list = ['terrified', 'unfriendly', 'sad', 'serious', 'shouting', 'affectionate', 'angry',             
+                'embarrassed', 'empathetic', 'envious', 'excited', 'fearful', 'friendly', 
+                'gentle', 'hopeful', 'whispering', 'lyrical', 'calm', 'chat', 'cheerful', 
+                'depressed', 'disgruntled', 'poetry-reading', 'customerservice', 'assistant', 
+                'advertisement_upbeat', 'documentary-narration', 'narration-professional', 
+                'narration-relaxed', 'newscast', 'newscast-casual', 'newscast-formal', 
+                'sports_commentary', 'sports_commentary_excited'
+            ]
+
+emotion_degree = ['0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1.0', '1.1', 
+                  '1.2', '1.3', '1.4', '1.5', '1.6', '1.7', '1.8', '1.9', '2.0'
+            ]
+
+# helper function for ssml updates
 def remove_namespace_prefixes(element, keep_mstts=False):
     for child in element.iter():
         if "}" in child.tag:
@@ -72,143 +153,6 @@ xmlns:mstts="https://www.w3.org/2001/mstts">
 """
 
 
-all_voice = [
-    'en-US-AnaNeural',
-    'en-AU-CarlyNeural',
-    'en-GB-MaisieNeural',
-    'here starts FEMALE',
-    'en-US-JaneNeural',
-    'en-US-JennyNeural', 
-    'en-US-MichelleNeural', 
-    'en-US-NancyNeural',   
-    'en-US-SaraNeural', 
-    'en-AU-FreyaNeural', 
-    'en-AU-KimNeural', 
-    'en-AU-NatashaNeural', 
-    'en-GB-LibbyNeural',
-    'en-GB-SoniaNeural',
-    'en-HK-YanNeural',
-    'here starts MALE',
-    'en-US-AIGenerate1Neural',
-    'en-US-BrandonNeural',
-    'en-US-ChristopherNeural',
-    'en-US-DavisNeural',
-    'en-US-EricNeural',
-    'en-US-GuyNeural',
-    'en-US-JacobNeural',
-    'en-US-RogerNeural',
-    'en-CA-LiamNeural',
-    'en-US-JasonNeural',
-    'en-US-SteffanNeural', 
-    'en-US-TonyNeural',
-    'en-AU-NeilNeural', 
-    'en-AU-WilliamNeural',
-    'en-GB-OliverNeural', 
-    'en-GB-RyanNeural',
-    'here starts CHINESE',
-    'zh-CN-sichuan-YunxiNeural',
-    'zh-CN-shandong-YunxiangNeural',
-    'zh-CN-shaanxi-XiaoniNeural',
-    'zh-CN-liaoning-XiaobeiNeural',
-    'zh-CN-henan-YundengNeural',
-    'zh-CN-XiaochenNeural',
-    'zh-CN-XiaohanNeural',
-    'zh-CN-XiaomengNeural',
-    'zh-CN-XiaomoNeural',
-    'zh-CN-XiaoqiuNeural',
-    'zh-CN-XiaoruiNeural',
-    'zh-CN-XiaoshuangNeural',
-    'zh-CN-XiaoxiaoNeural',
-    'zh-CN-XiaoxuanNeural',
-    'zh-CN-XiaoyanNeural',
-    'zh-CN-XiaoyiNeural',
-    'zh-CN-XiaoyouNeural',
-    'zh-CN-XiaozhenNeural',
-    'zh-CN-YunfengNeural',
-    'zh-CN-YunhaoNeural',
-    'zh-CN-YunjianNeural',
-    'zh-CN-YunxiaNeural',
-    'zh-CN-YunxiNeural',
-    'zh-CN-YunyangNeural',
-    'zh-CN-YunyeNeural',
-    'zh-CN-YunzeNeural'
-]
-
-
-which_speed_to_use = [
-    '0.5',
-    '0.7',
-    '0.8',
-    '0.9',
-    '1.0',
-    '1.2',
-    '1.4',
-    '1.6',
-    '1.8',
-    '2.0'
-]
-
-emotion_list = [
-    'terrified', 
-    'unfriendly', 
-    'sad', 
-    'serious', 
-    'shouting', 
-    'affectionate', 
-    'angry',             
-    'embarrassed', 
-    'empathetic', 
-    'envious', 
-    'excited', 
-    'fearful', 
-    'friendly', 
-    'gentle', 
-    'hopeful',             
-    'whispering', 
-    'lyrical', 
-    'calm', 
-    'chat', 
-    'cheerful', 
-    'depressed', 
-    'disgruntled', 
-    'poetry-reading', 
-    'customerservice', 
-    'assistant', 
-    'advertisement_upbeat', 
-    'documentary-narration', 
-    'narration-professional', 
-    'narration-relaxed', 
-    'newscast', 
-    'newscast-casual', 
-    'newscast-formal', 
-    'sports_commentary', 
-    'sports_commentary_excited'
-]
-
-
-emotion_degree = [
-    '0.1',
-    '0.2',
-    '0.3',
-    '0.4',
-    '0.5',
-    '0.6',
-    '0.7',
-    '0.8',
-    '0.9',
-    '1.0',
-    '1.1',
-    '1.2',
-    '1.3',
-    '1.4',
-    '1.5',
-    '1.6',
-    '1.7',
-    '1.8',
-    '1.9',
-    '2.0'
-]
-
 # azure tts api config
 speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=None)
@@ -218,6 +162,40 @@ def generate_audio(updated_ssml, file_name):
     result = speech_synthesizer.speak_ssml_async(updated_ssml).get()
     stream = speechsdk.AudioDataStream(result)
     stream.save_to_wav_file(file_name)   
+
+
+def play_audio(filename):
+    """
+    Play an audio file with a given filename using PyAudio and wave libraries.
+    
+    Parameters:
+    filename (str): The file name of the audio file to be played.
+    """
+    # Open the file
+    wf = wave.open(filename, 'rb')
+    
+    # Create an audio object
+    p = pyaudio.PyAudio()
+    
+    # Open stream
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+    
+    # Read and play
+    data = wf.readframes(1024)
+    while data:
+        stream.write(data)
+        data = wf.readframes(1024)
+    
+    # Stop stream
+    stream.stop_stream()
+    stream.close()
+    
+    # Close PyAudio
+    p.terminate()
+
 
 
 # Command line arguments parsing
@@ -242,9 +220,13 @@ output_file = args.output_file
 
 # adjust voice properties
 updated_ssml = modify_ssml(ssml, voice_name, voice_name, emotion, emotion_degree, radio_speed, "", text)
-print(updated_ssml)
+#print(updated_ssml)
               
 generate_audio(updated_ssml, "output.wav")    
+
+# play the generated audio
+play_audio('output.wav')
+
        
 
 
