3b06bd2c6b21334b9bfc81cc3246cc416c8cffcb
xu dong <yanxuceo@gmail.com>
2023-08-18 23:36:28 +0200

    made the testing code into a get_transcription function

--- raspberryPi/pi_whisper_api.py
+++ raspberryPi/pi_whisper_api.py
@@ -6,81 +6,86 @@ import wave
 from pydub import AudioSegment
 
 
-# Set your OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
+def get_transcription_from_whisper():
+    # Set your OpenAI API key
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Set the audio parameters
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
-CHUNK = 2048
-SILENCE_THRESHOLD = 500  # Silence threshold
-SPEECH_END_TIME = 1.0  # Time of silence to mark the end of speech
+    # Set the audio parameters
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 16000
+    CHUNK = 2048
+    SILENCE_THRESHOLD = 300  # Silence threshold
+    SPEECH_END_TIME = 1.0  # Time of silence to mark the end of speech
 
-# Initialize PyAudio
-audio = pyaudio.PyAudio()
+    # Initialize PyAudio
+    audio = pyaudio.PyAudio()
 
-# Start Recording
-stream = audio.open(format=FORMAT, channels=CHANNELS,
-                    rate=RATE, input=True,
-                    frames_per_buffer=CHUNK)
+    # Start Recording
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=CHUNK)
 
-print("Recording...Waiting for speech to begin.")
+    print("Recording...Waiting for speech to begin.")
 
-frames = []
-silence_frames = 0
-is_speaking = False
-total_frames = 0
+    frames = []
+    silence_frames = 0
+    is_speaking = False
+    total_frames = 0
 
-while True:
-    data = stream.read(CHUNK)
-    frames.append(data)
-    total_frames += 1
-    
-    # Convert audio chunks to integers
-    audio_data = np.frombuffer(data, dtype=np.int16)
-    
-    # Check if user has started speaking
-    if np.abs(audio_data).mean() > SILENCE_THRESHOLD:
-        is_speaking = True
+    while True:
+        data = stream.read(CHUNK)
+        frames.append(data)
+        total_frames += 1
+        
+        # Convert audio chunks to integers
+        audio_data = np.frombuffer(data, dtype=np.int16)
+        
+        # Check if user has started speaking
+        if np.abs(audio_data).mean() > SILENCE_THRESHOLD:
+            is_speaking = True
 
-    # Detect if the audio chunk is silence
-    if is_speaking:
-        if np.abs(audio_data).mean() < SILENCE_THRESHOLD:
-            silence_frames += 1
-        else:
-            silence_frames = 0
+        # Detect if the audio chunk is silence
+        if is_speaking:
+            if np.abs(audio_data).mean() < SILENCE_THRESHOLD:
+                silence_frames += 1
+            else:
+                silence_frames = 0
 
-    # End of speech detected
-    if is_speaking and silence_frames > SPEECH_END_TIME * (RATE / CHUNK):
-        print("End of speech detected.")
-        break
+        # End of speech detected
+        if is_speaking and silence_frames > SPEECH_END_TIME * (RATE / CHUNK):
+            print("End of speech detected.")
+            break
 
-# Stop Recording
-stream.stop_stream()
-stream.close()
-audio.terminate()
+    # Stop Recording
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
 
-print("Finished recording.")
+    print("Finished recording.")
+    combined_audio_data = b''.join(frames)
 
-combined_audio_data = b''.join(frames)
+    # Convert raw data to an AudioSegment object
+    audio_segment = AudioSegment(
+        data=combined_audio_data,
+        sample_width=audio.get_sample_size(FORMAT),
+        frame_rate=RATE,
+        channels=CHANNELS
+    )
 
-# Convert raw data to an AudioSegment object
-audio_segment = AudioSegment(
-    data=combined_audio_data,
-    sample_width=audio.get_sample_size(FORMAT),
-    frame_rate=RATE,
-    channels=CHANNELS
-)
+    # Export as a compressed MP3 file with a specific bitrate
+    audio_segment.export("output_audio_file.mp3", format="mp3", bitrate="32k")
 
-# Export as a compressed MP3 file with a specific bitrate
-audio_segment.export("output_audio_file.mp3", format="mp3", bitrate="32k")
+    # Open the saved file to send to the API
+    with open("output_audio_file.mp3", "rb") as f:
+        transcript = openai.Audio.transcribe("whisper-1", f)
 
-
-# Open the saved file to send to the API
-with open("output_audio_file.mp3", "rb") as f:
-    transcript = openai.Audio.transcribe("whisper-1", f)
+    # Return the transcript text
+    return transcript['text']
 
 
-# Print the transcript
-print("Transcript:", transcript['text'])
+
+# Example usage of the function
+if __name__ == '__main__':
+    transcription = get_transcription_from_whisper()
+    print("Transcript:", transcription)
