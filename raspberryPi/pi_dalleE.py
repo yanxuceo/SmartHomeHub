cb7336cede32fe2e54695705bb43c00b04ed92e8
xu dong <yanxuceo@gmail.com>
2023-08-23 20:21:19 +0200

    updated to save Dall-E generated image

--- raspberryPi/pi_dalleE.py
+++ raspberryPi/pi_dalleE.py
@@ -1,18 +1,37 @@
+import openai
 import os
 
-import openai
-import json
+import requests
 
-# Set your OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-response = openai.Image.create(
-  prompt="a ship in the ocean",
-  n=1,
-  size="512x512"
-)
+def generate_image(prompt_text):
+    # Use the OpenAI API to generate an image based on the given prompt
+    response = openai.Image.create(
+      prompt=prompt_text,
+      n=1,
+      size="512x512"
+    )
+
+    image_url = response['data'][0]['url']
+    
+    # Use requests to download the image
+    image_response = requests.get(image_url)
+
+    # Check if the request was successful
+    image_response.raise_for_status()
+
+    # Save the image locally
+    local_image_name = "gen.png"
+    with open(local_image_name, 'wb') as file:
+        file.write(image_response.content)
+
+    print(f"Image saved as {local_image_name}")
+    return local_image_name
 
 
-image_url = response['data'][0]['url']
-print(image_url)
+
+# Example usage
+if __name__ == "__main__":
+    local_image_path = generate_image("a ship in the ocean")
